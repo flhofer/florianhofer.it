2aba748fb311817517a215cbf9cf341555444533
Florian Hofer <info@florianhofer.it>
2019-11-25 16:01:53 -0800

    version 1 -> start page

---
+++ cgi-bin/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+void main () {
+    printf ("Content-Type: text/html\n\n"
+			"<html><head><title>www.florianhofer.it</title></head>\n"
+			"<body>This page is temporarly offline. (check back in a month)\n"
+			"<br><br><br>\n"
+			"<small>This web page is proudly made using C</small>\n"
+			"</body><html>\n");
+}
