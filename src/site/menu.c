fea910763e7c2de54f2c1bcbba439459492f2dcd
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2019-12-14 11:20:45 +1100

    added link to git to main page

--- src/site/menu.c
+++ src/site/menu.c
@@ -7,7 +7,8 @@
 int main () {
 	cgiHeader();
     (void)printf ("<div style=\"padding-left:16px\">\n"
-    		"<p>This page is temporarly offline. (check back in a month)</p>\n"
+    		"<h2>This page is temporarly offline.</h2>\n"
+    		"<p>Check back in a month or follow development <a href=\"https://github.com/flhofer/florianhofer.it\">here</a></p>\n"
     		"</div>\n");
 //    printf("%s\n", sqlite3_libversion());
     cgiFooter();
